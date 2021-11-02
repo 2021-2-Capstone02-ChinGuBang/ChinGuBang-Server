91242828e183ebd8fc2c250d598082cf215d18c5
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-11-03 01:04:36 +0900

    [fix] package.json main

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import express from "express"; 
+import express from "express";
 import { sequelize } from "./models";
 import router from "./router";
 
-const app = express(); 
+const app = express();
 
 sequelize.sync({ force: false }).catch((error) => {
-    console.error(error);
-  });
-  
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());  
+  console.error(error);
+});
 
-app.use('/', router);   //라우터 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", router); //라우터
 // error handler
 app.use(function (err, req, res, next) {
-
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "production" ? err : {};
 
@@ -23,14 +22,14 @@ app.use(function (err, req, res, next) {
   res.json({ error: err });
 });
 
-app 
+app
   .listen(process.env.PORT, () => {
     console.log(`
     ################################################
     🛡️  Server listening on port: ${process.env.PORT} 🛡️
     ################################################
   `);
-  sequelize
+    sequelize
       // .sync({ alter: true })
       .authenticate()
       .then(async () => {
